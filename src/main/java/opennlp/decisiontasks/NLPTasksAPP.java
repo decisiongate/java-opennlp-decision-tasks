6f9617df42c0d0fe3d99752ce9da3fb3c2033af6
decisiongate <decisiongate.com@gmail.com>
2020-08-18 11:06:06 +0200

    java-opennlp-decision-tasks

---
+++ src/main/java/opennlp/decisiontasks/NLPTasksAPP.java
@@ -0,0 +1,16 @@
+package opennlp.decisiontasks;
+
+import java.io.IOException;
+
+import static opennlp.decisiontasks.OpenNLP.*;
+
+public class NLPTasksAPP {
+    public static void main(String[] args) {
+        try { SentenceDetection(); } catch (IOException ioException) { ioException.printStackTrace(); }
+        try { Tokenize(); } catch (IOException ioException) { ioException.printStackTrace(); }
+        try { findName(); } catch (IOException ioException) { ioException.printStackTrace(); }
+        try { POSTag(); } catch (IOException ioException) { ioException.printStackTrace(); }
+        try { chunk(); } catch (IOException ioException) { ioException.printStackTrace(); }
+        try { Parse(); } catch (IOException ioException) { ioException.printStackTrace(); }
+    }
+}
